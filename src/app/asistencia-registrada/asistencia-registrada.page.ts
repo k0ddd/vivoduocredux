46f325cd9302421619bbcec4280c2068242a336b
k0ddd <benja.cepe123@gmail.com>
2024-09-03 17:47:58 -0400

    SYSDATE AGREGADO
    Co-authored-by: Flvrd-hub <Flvrd-hub@users.noreply.github.com>

--- src/app/asistencia-registrada/asistencia-registrada.page.ts
+++ src/app/asistencia-registrada/asistencia-registrada.page.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+  import { Component, OnInit } from '@angular/core';
 
-@Component({
-  selector: 'app-asistencia-registrada',
-  templateUrl: './asistencia-registrada.page.html',
-  styleUrls: ['./asistencia-registrada.page.scss'],
-})
-export class AsistenciaRegistradaPage implements OnInit {
+  @Component({
+    selector: 'app-asistencia-registrada',
+    templateUrl: './asistencia-registrada.page.html',
+    styleUrls: ['./asistencia-registrada.page.scss'],
+  })
+  export class AsistenciaRegistradaPage implements OnInit {
+    fecha: string = '';
+    hora: string = '';
+    
+    constructor() { }
 
-  constructor() { }
+    ngOnInit() {
+      const now = new Date();
+      const utcOffset = -4; 
+      const localDate = new Date(now.getTime() + (utcOffset * 60 * 60 * 1000));
 
-  ngOnInit() {
+
+      this.fecha = localDate.toISOString().slice(0, 10); 
+
+      this.hora = localDate.toISOString().slice(11, 16)
+    }
+
   }
-
-}
