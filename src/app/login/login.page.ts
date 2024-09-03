c87d0aca2c8c6a204b779244834529d532b8c6f3
k0ddd <benja.cepe123@gmail.com>
2024-09-02 20:37:47 -0400

    arreglo de interfaz, botones añadidos, animacion de validacion
    Co-authored-by: Flvrd-hub <Flvrd-hub@users.noreply.github.com>

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -23,35 +23,67 @@ export class LoginPage implements OnInit {
 
   ingresar(){
     if(this.validateModel(this.login)){
-      this.presentToast("Bienvenido "+this.login.Usuario);
-      
-      let NavigationExtras:NavigationExtras={
-        state: {user:this.login.Usuario}
-      };
-      this.router.navigate(['home'], NavigationExtras);
-    } else {
-      this.presentToast("falta: "+this.field);
-   }
-   
+      if (this.validateLongUsuario(this.login.Usuario)){
+        if(this.validateLongPass(this.login.Password)){
+          this.presentToast("Bienvenido "+this.login.Usuario);
+          let navigationExtras:NavigationExtras={
+            state: {user:this.login.Usuario} 
+          }
+          this.router.navigate(['home'],navigationExtras);
+        }else{
+          this.presentToast("La contraseña debe ser de largo 4 y solo numerica");
+          this.login.Password="";
+        }
+      }else{
+        this.presentToast("El largo del usuario debe ser entre 3 y 8 caracteres");
+        this.login.Usuario="";
+      }
+    }
+    else{
+      this.presentToast("Falta: "+this.field);
+    }
   }
 
   validateModel(model:any){
-    for (var [key, value] of Object.entries(model)){
-      if (value ==""){
-        this.field = key;
+    // Recorro todas las entradas que me entrega Object entries y obtengo su clave, valor
+    for (var [key, value] of Object.entries(model)) {
+      // Si un valor es "" se retornara false y se avisara de lo faltante
+      if (value=="") {
+        // Se asigna el campo faltante
+        this.field=key;
+        // Se retorna false
         return false;
       }
     }
     return true;
   }
-    
-    async presentToast(message:string, duration?:number){
-      const toast = await this.toastController.create({
-        message:message,
-        duration: duration ? duration : 2000
 
-      });
-    
+  validateLongUsuario(dato:String){
+    if(dato.length>=3 && dato.length<=8){
+      return true
     }
+    return false;
+  }
+  validateLongPass(dato:String){
+    if(dato.length==4 && Number.isInteger(Number(dato))){
+      return true;
+    }
+    return false;
+  }
+    
+  /**
+   * Muestra un toast al usuario
+   * @param message Mensaje a presentar al usuario
+   * @param duration Duración el toast, este es opcional
+   */
+  async presentToast(message:string, duration?:number){
+    const toast = await this.toastController.create(
+      {
+        message:message,
+        duration:duration?duration:2000
+      }
+    );
+    toast.present();
+  }
 }
 
--- src/app/recuperar-contrasena/recuperar-contrasena.page.ts
+++ src/app/recuperar-contrasena/recuperar-contrasena.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-recuperar-contrasena',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecuperarContrasenaPage implements OnInit {
 
-  constructor() { }
+  constructor(public alertCrtl: AlertController) { }
 
   ngOnInit() {
+  }
+
+  async presentAlert(){
+    const alert = await this.alertCrtl.create({
+      header: 'CORREO ENVIADO',
+      message: 'Se ha enviado un codigo a su correo electronico',
+      buttons: ['OK']
+    });
+
+    await alert.present();
   }
 
 }
