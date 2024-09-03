09f71db58e8c9cc8d1710d569d0c5079979a66aa
k0ddd <benja.cepe123@gmail.com>
2024-09-03 19:10:24 -0400

    listo, falta el anadir animaciones y alerta de sesion cerrada

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  usuario: string = '';
 
-  constructor() {}
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    const navigation = this.router.getCurrentNavigation();
+    if (navigation && navigation.extras.state) {
+      const state = navigation.extras.state as { user: string };
+      this.usuario = state.user || 'Usuario no encontrado';
+    }
+  }
+
+  goToAlumno() {
+    const navigationExtras = {
+      state: {
+        user: this.usuario
+      }
+    };
+    this.router.navigate(['/alumno'], navigationExtras);
+  }
+
+  goToPaginaD() {
+    const navigationExtras = {
+      state: {
+        user: this.usuario
+      }
+    };
+    this.router.navigate(['/pagina-docente'], navigationExtras);
+  }
 
 }
+
+
--- src/app/pagina-docente/pagina-docente.page.ts
+++ src/app/pagina-docente/pagina-docente.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pagina-docente',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pagina-docente.page.scss'],
 })
 export class PaginaDocentePage implements OnInit {
+  usuario: string = '';
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    const navigation = this.router.getCurrentNavigation();
+    if (navigation && navigation.extras.state) {
+      const state = navigation.extras.state as { user: string };
+      this.usuario = state.user || 'Usuario no encontrado';
+    } else {
+      this.usuario = 'Usuario no encontrado';
+    }
   }
-
 }
